96aaf1c6d17757695742d2f84324f39bca12d1f0
Xinran Ma <xinran_ma@hotmail.com>
2019-09-08 20:06:03 +1000

    Updates

---
+++ precache-manifest.986b624a86a9678c1add867824391201.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b8279e2a119e899bf42c22c9eafebe7",
+    "url": "/weather-panel/index.html"
+  },
+  {
+    "revision": "9a7d1ada732c1c8841fb",
+    "url": "/weather-panel/static/css/main.45172327.chunk.css"
+  },
+  {
+    "revision": "e3dade205817acef03bd",
+    "url": "/weather-panel/static/js/2.bd7e79bd.chunk.js"
+  },
+  {
+    "revision": "9a7d1ada732c1c8841fb",
+    "url": "/weather-panel/static/js/main.48204441.chunk.js"
+  },
+  {
+    "revision": "c0fd40c0aa725cf9e8e5",
+    "url": "/weather-panel/static/js/runtime~main.88a28516.js"
+  }
+]);
